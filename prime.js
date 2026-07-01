05e179f79ad3afa1a124aacbee9155313bafa4e1
Muthukrishnan68 <muthubkrishnan68@gmail.com>
2026-07-01 09:49:18 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -4,12 +4,23 @@
  * @returns {boolean} True if prime, false otherwise.
  */
 const isPrime = (num) => {
-    // TODO: 1. Handle edge cases (numbers <= 1) using const rules where applicable
-    
-    // TODO: 2. Handle the number 2 and even numbers
-    
-    // TODO: 3. Use a loop to check for factors up to the square root of num
-    // Hint: Use 'const' for values that stay the same and 'let' for values that change
+    if (nun<=1{
+        return false;
+    }
+    for(let i=2;< num;++){
+        if(num % i === 0){
+            returnfalse;
+        }
+    }
+    return true;
+}
+let number=17;
+if(isprime(number)){
+    console.log(number +"is a prime number");
+}
+else{
+    console.log(number +" is not a prime number");
+}
     
     return false; // Change this return statement
 };
